62f3b6fb03920e338cc1dd4f23eaf3a1a94406be
ilya <zapaska96@gmail.com>
2022-09-15 10:24:29 +0500

    chef компонент, без кÐовычек, ьбез адаптива

---
+++ src/components/Chef.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Paragraph, SubTitle, Title } from './index.js';
+
+const Chef = () => {
+    return (
+        <section>
+            <div
+                className={
+                    'bg-[url("src/assets/bg.png")] bg-center bg-no-repeat bg-cover '
+                }
+            >
+                <div
+                    className={
+                        'min-h-screen max-w-[1300px] mx-auto flex justify-center items-center  gap-[96px]'
+                    }
+                >
+                    <div className={'flex-1'}>
+                        <img src={'src/assets/chef.png'} alt={'chef picture'} />
+                    </div>
+                    <div
+                        className={
+                            'flex-1 flex flex-col justify-between items-start gap-16'
+                        }
+                    >
+                        <div>
+                            <SubTitle text={'Chef’s Word'} />
+                            <Title text={'What we believe in'} />
+                        </div>
+                        <div>
+                            <Paragraph
+                                text={
+                                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit . auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc.'
+                                }
+                                style={'italic'}
+                            />
+                        </div>
+                        <div>
+                            <h3
+                                className={
+                                    'font-cormorant text-primary font-normal text-[2rem] leading-[42px] tracking-wider'
+                                }
+                            >
+                                Kevin Luo
+                            </h3>
+                            <h4
+                                className={
+                                    'font-openSans text-dimWhite font-normal text-base tracking-wider'
+                                }
+                            >
+                                Chef & Founder
+                            </h4>
+                        </div>
+                        <div className={'w-[235px]'}>
+                            <img
+                                src={'src/assets/sign.png'}
+                                alt={"chef's sign"}
+                                className={'w-full object-contain'}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Chef;
